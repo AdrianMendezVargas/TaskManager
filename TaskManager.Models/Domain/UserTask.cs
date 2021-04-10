1ab8a35e2bb1625cf163fe6f45a66028118125e9
Adrian Alcides Mendez Vargas <adrian_mendez@ucne.edu.do>
2021-04-10 16:49:02 -0400

    creating TaskRepositoty with it's interface. creating BaseService. Renaming Task to UserTask

---
+++ TaskManager.Models/Domain/UserTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using TaskManager.Models.Domain.Enums;
+
+namespace TaskManager.Models.Domain {
+    public class UserTask : Record {
+
+        [Required]
+        [StringLength(maximumLength:30, MinimumLength = 1, ErrorMessage = "Invalid task name")]
+        public string Name { get; set; }
+        public TaskState State { get; set; }
+
+    }
+}
---
+++ TaskManager.Repository/ITaskRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Models.Domain;
+
+namespace TaskManager.Repository {
+    public interface ITaskRepository {
+        Task CreateAsync(UserTask task);
+        Task<UserTask> GetByIdAsync(int id);
+        void Remove(UserTask task);
+        void Update(UserTask task);
+
+    }
+}
