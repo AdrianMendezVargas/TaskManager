e8aed0c5b79f8c08dfccec200a25e3deff65aa8a
Adrian Alcides Mendez Vargas <adrian_mendez@ucne.edu.do>
2021-04-13 17:58:23 -0400

    Creating Unit test for the service: TaskService.CreateTaskAsync

---
+++ TaskManager.Services.Tests/TaskServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Services.Tests {
+    [TestClass()]
+    public class TaskServiceTests {
+        [TestMethod()]
+        public void CreateTaskAsyncTest() {
+            Assert.Fail();
+        }
+    }
+}
---
+++ TaskManager.Tests/TaskServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Repository;
+using TaskManager.Models.Domain;
+using TaskManager.Models.Domain.Enums;
+
+namespace TaskManager.Tests {
+    [TestClass()]
+    public class TaskServiceTests {
+
+        private TaskService TaskService;
+
+        [TestInitialize]
+        public void Initialize() {
+            var db = ApplicacionDbInitializer.GetDbContext();
+            var unit = new EfUnitOfWork(db);
+            TaskService = new TaskService(unit);
+        }
+
+        [TestMethod()]
+        public async Task CreateTaskAsyncTest() {
+            //Arrange
+            UserTask task = new UserTask {
+                Name = "Tarea 1" ,
+                State = TaskState.NotStarted ,
+                CreatedOn = DateTime.Now
+            };
+
+            //Action
+            var result = await TaskService.CreateTaskAsync(task);
+
+            //Asure
+            Assert.IsTrue(result.IsSuccess);
+        }
+    }
+}
